95f191e69841a7146fc5a0293550b75ac579af8e
Harsh Singh <harshsngh07@gmail.com>
2020-12-18 13:11:13 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+#!usr/bin/env node
+
+const inquirer = require("inquirer");
+const shell = require("shelljs");
+const figlet = require("figlet");
+const chalk = require("chalk");
+
+const init = () => {
+  console.log(
+    chalk.green(
+      figlet.textSync("Node f***king js", {
+        font: "Ghost",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+      })
+    )
+  );
+};
+
+const askQuestions = () => {
+  const questions = [
+    {
+      name: "FILENAME",
+      type: "input",
+      message: "What is the name of the extension?",
+    },
+    {
+      type: "list",
+      name: "EXTENSION",
+      message: "What is the file extension?",
+      choices: [".rb", ".cpp", ".js", ".py"],
+      filter: function (val) {
+        return val.split(".")[1];
+      },
+    },
+  ];
+  return inquirer.prompt(questions);
+};
+
+const createFile = (filename, extension) => {
+  const filePath = `${process.cwd()}/${filename}.${extension}`;
+  shell.touch(filePath);
+  return filePath;
+};
+
+const success = (filePath) => {
+  console.log(chalk.white.bgGreen.bold(`Done! File created at ${filePath}`));
+};
+
+const run = async () => {
+  // show script introduction
+  init();
+
+  // ask questions
+  const answers = await askQuestions();
+  const { FILENAME, EXTENSION } = answers;
+
+  // create the file
+  const filePath = createFile(FILENAME, EXTENSION);
+
+  // show success message
+  success(filePath);
+};
+
+run();
